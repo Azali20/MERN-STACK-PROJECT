011823e52f703906b9a6b32b53fc58c18fdbc204
Azali.20 <clintonfaraday@gmail.com>
2023-08-24 06:43:10 +0100

    MERN STACK PROJECT

---
+++ server/routes/posts.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+import { getPosts, getPostsBySearch, getPostsByCreator, getPost, createPost, updatePost, likePost, commentPost, deletePost } from '../controllers/posts.js';
+
+
+import auth from "../middleware/auth.js";
+
+const router = express.Router();
+
+// This means that the routes is reached by going to local host 5000/posts. Because we added a prefix of post to all routes in here.
+//http://localhost:5000/posts
+
+router.get('/creator', getPostsByCreator);
+router.get('/search', getPostsBySearch);
+router.get('/', getPosts);
+router.get('/:id', getPost);
+
+router.post('/', auth,  createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost', commentPost);
+
+export default router;
